e01cc44dbedec435cd6a5f497a18e7940228d7a1
frank <duanxitaoa@126.com>
2018-05-20 13:32:55 +0800

    init project

---
+++ phoenix/src/main/java/com/qit/App.java
@@ -0,0 +1,13 @@
+package com.qit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
